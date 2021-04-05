13a1bb34a96324c05ca6bd0f1428dec03f330780
andrii.denysenko <andrii.denysenko@4shared.com>
2021-04-05 21:52:58 +0300

    PWA - add code to sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const cacheName = 'sw-cache-v1';
+const cacheName = 'sw-cache-v2';
 const appShellFiles = [
     '/index.html',
     '/icons/512x512.png'
 ];
 
 self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
+    console.log('[Service Worker] Install ' + cacheName);
     e.waitUntil((async () => {
         const cache = await caches.open(cacheName);
         console.log('[Service Worker] Caching all: app shell and content');
@@ -17,7 +17,9 @@ self.addEventListener('fetch', (e) => {
     e.respondWith((async () => {
         const r = await caches.match(e.request);
         console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-        if (r) { return r; }
+        if (r) {
+            return r;
+        }
         const response = await fetch(e.request);
         const cache = await caches.open(cacheName);
         console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
@@ -29,7 +31,9 @@ self.addEventListener('fetch', (e) => {
 self.addEventListener('activate', (e) => {
     e.waitUntil(caches.keys().then((keyList) => {
         Promise.all(keyList.map((key) => {
-            if (key === cacheName) { return; }
+            if (key === cacheName) {
+                return;
+            }
             caches.delete(key);
         }))
     })());
